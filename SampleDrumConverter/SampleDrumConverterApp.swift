79df54c08ca7b81f4d0274779ca78653d7daf678
Jarl Lyng <38857800+JarlLyng@users.noreply.github.com>
2024-12-27 00:05:43 +0100

    Initial Commit

---
+++ SampleDrumConverter/SampleDrumConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDrumConverterApp.swift
+//  SampleDrumConverter
+//
+//  Created by Jarl Lyng on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SampleDrumConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
